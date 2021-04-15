4f19c4d72e7c86153421c7565f6aa97368c44a17
Joel Segal <joel.segal@intranda.com>
2021-04-15 15:04:36 +0200

    initial

--- plugin/src/main/java/de/intranda/goobi/plugins/YerushaAim25WorkflowPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/YerushaAim25WorkflowPlugin.java
@@ -11,10 +11,10 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 @PluginImplementation
 @Log4j2
-public class SampleWorkflowPlugin implements IWorkflowPlugin, IPlugin {
+public class YerushaAim25WorkflowPlugin implements IWorkflowPlugin, IPlugin {
     
     @Getter
-    private String title = "intranda_workflow_sample";
+    private String title = "intranda_workflow_yerusha_aim25";
         
     @Getter
     private String value;
@@ -26,14 +26,14 @@ public class SampleWorkflowPlugin implements IWorkflowPlugin, IPlugin {
 
     @Override
     public String getGui() {
-        return "/uii/plugin_workflow_sample.xhtml";
+        return "/uii/plugin_workflow_yerusha_aim25.xhtml";
     }
 
     /**
      * Constructor
      */
-    public SampleWorkflowPlugin() {
-        log.info("Sample workflow plugin started");
+    public YerushaAim25WorkflowPlugin() {
+        log.info("YerushaAim25 workflow plugin started");
         value = ConfigPlugins.getPluginConfig(title).getString("value", "default value");
     }
 }
